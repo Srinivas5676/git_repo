925901e84dc389744a7c533f149f3f7caa25a006
SRINIVAS\srini <srinivasvadla800@gmail.com>
2023-08-16 17:28:36 +0530

    I am going to do First Commit new!

--- Test123.java
+++ Test123.java
@@ -12,7 +12,7 @@ public class Test123
 public static void main(String args[]){
 System.out.println("Hello");
 System.out.println("I am good");
-System.out.println("good");
+System.out.println("good now");
 }
 >>>>>>> 6f94f70 (I am going to do First Commit!)
 }
